303a56c7485045aa5a0db80333a7e61258869d9c
debasish <debasish.sahoo96@gmail.com>
2025-02-27 17:05:11 +0530

    Services Component is Done _

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,24 +7,31 @@ import MenuLink from "./components/MenuLink";
 import Stats from "./components/Stats";
 import "./App.css";
 function App() {
+  const HeaderData = {
+    title: "Changing the World Through Coding",
+    desc: "This is My Second Project on React",
+  };
 
-const HeaderData={
-  title:"Changing the World Through Coding",
-  desc:"This is My Second Project on React"
-}
+  const ContentData = {
+    title: "Our Agency, our selves.",
+    desc: "This is a Description of Content",
+    imgUrl: "https://dummyimage.com/600x400/000/fff",
+    altText: "Agency Image",
+  };
 
-const ContentData={
-  title:"Our Agency, our selves.",
-  desc:"This is a Description of Content",
-  imgUrl:"https://dummyimage.com/600x400/000/fff",
-  altText:"Agency Image"
-}
+  const ServicesData = [
+    { title: "Photoshop", desc: "Expert photo editing and designing services." },
+    { title: "Javascript", desc: "Building interactive and dynamic web applications." },
+    { title: "Marketing", desc: "Strategic marketing solutions to boost your brand." },
+    
+  ];
+
   return (
     <>
       <div className="container">
-        <Header headerProps={HeaderData}/>
-        <Content contentProps={ContentData}/>
-        <Services />
+        <Header headerProps={HeaderData} />
+        <Content contentProps={ContentData} />
+        <Services servicesProps={ServicesData}/>
         <Stats />
         <hr />
         <div className="row column">
