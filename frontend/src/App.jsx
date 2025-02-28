5fa54ac34db8a5ba879478e1e88fb208acb94cab
debasish <debasish.sahoo96@gmail.com>
2025-02-28 16:38:47 +0530

    update -2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import PropsBasic from "./components/01-PropsBasic";
 import ValueProps from "./components/02-ValueProps";
 import "./App.css";
 function App() {
+let dType='template'+2.0
+let anyData="This"+"is Javascript"+{dType}+"Called template"
   return (
     <>
       <PropsBasic name="Debasish" title="Sahoo" />
@@ -13,6 +15,7 @@ function App() {
       objData={{name:"Debasish", title:"Sahoo"}}
       arrData={[1,2,3,4,5,6,7,8,9,10]}
       jsxData={2+2+2+14-3-(2/3)*(23*5)}
+      anyData={anyData}
       />
     </>
   );
