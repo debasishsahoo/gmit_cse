461d222c2eef927ce0f3244b26353918d7aae790
debasish <debasish.sahoo96@gmail.com>
2025-03-07 15:19:24 +0530

    Update user.services.js

--- backend/services/user.services.js
+++ backend/services/user.services.js
@@ -2,9 +2,16 @@ const userModel=require('../models/user.model')
 
 
 
-
 const userServices={
-    INSERT:async ()=>{},
+    INSERT:async (userPayload)=>{
+        const { email } = userPayload;
+
+    // Check if user already exists
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) throw new Error("User already exists");
+
+    return await userModel.create(userPayload);
+    },
     VIEW_ALL:async()=>{},
     VIEW_SINGLE:async()=>{},
     UPDATE:async()=>{},
