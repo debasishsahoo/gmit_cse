98cff31202fb5b7bfd52703435ca7d886772b292
debasish <debasish.sahoo96@gmail.com>
2025-03-07 15:24:52 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,15 @@
 const userServices = require("../services/user.services");
 
 const userController = {
-  signUp: async (req, res) => {},
+  signUp: async (req, res) => {
+    try {
+      const user = await userServices.INSERT(req.body);
+      res.status(201).json({ 
+      message: "User registered successfully", user })
+  } catch (error) {
+      res.status(400).json({ error: error.message });
+  }
+  },
   signIn: async (req, res) => {},
   signOut: async (req, res) => {},
   changePassword: async (req, res) => {},
