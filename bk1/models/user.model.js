9c49449a96a28111a1bc2b875fca3d4f97b150fe
debasish <debasish.sahoo96@gmail.com>
2025-03-07 11:45:58 +0530

    New Update

---
+++ bk1/models/user.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      lowercase: true,
+      minLength: [3, "Name must be at least 3 characters"],
+      maxLength: [32, "Name must be at most 32 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be at least 8 characters"],
+      trim: true,
+      select: false, // Prevent password from being queried
+    },
+    phoneNo: {
+      type: String,
+      default: null,
+      match: [/^\d{10}$/, "Phone number must be 10 digits"],
+    },
+  },
+  { timestamps: true } // Adds createdAt & updatedAt timestamps
+);
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Remove password before returning user object
+userSchema.methods.toJSON = function () {
+  const userObject = this.toObject();
+  delete userObject.password;
+  return userObject;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
