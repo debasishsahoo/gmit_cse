3c3ff2a9cfb2ba2a82bf4ee51b58746d25c1a5ff
debasish <debasish.sahoo96@gmail.com>
2025-03-11 15:13:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import "./App.css";
 function App() {
 
   return (
     <>
-    
+    <Login/>
+    <Signup/>
     </>
   );
 }
