3be37150730e18d04fcce228c143d3e0eb944a7a
debasish <debasish.sahoo96@gmail.com>
2025-03-11 16:52:21 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import { useState } from "react";
+import { signIn } from "../services/authServices";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [user, setUser] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signIn(user);
+      alert("login successful!");
+      navigate("/dashboard");
+    } catch (error) {
+      setError(error.response?.data?.error || "Login failed");
+    }
+  };
+
   return (
     <div>
       <h2>Login</h2>
-
-      <form>
-        <input type="email" name="email" placeholder="Email" required />
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
         <input
           type="password"
           name="password"
           placeholder="Password"
+          onChange={handleChange}
           required
         />
         <button type="submit">Signin</button>
