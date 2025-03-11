e63c4fea82e6bfcdca0d5ad25a13d71e57ff448d
debasish <debasish.sahoo96@gmail.com>
2025-03-11 15:48:28 +0530

    Navbar Added to App js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
 import "./App.css";
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <h1 className="logo">Product App</h1>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
