71bb15eeb89dd8e50040e967204ec6c5b9e0ac8e
debasish <debasish.sahoo96@gmail.com>
2025-03-11 14:51:02 +0530

    New Update

---
+++ frontend/src-3/App.jsx
@@ -0,0 +1,27 @@
+import PropsBasic from "./components/01-PropsBasic";
+import ValueProps from "./components/02-ValueProps";
+import PropsPass from './components/03-PropsPass'
+import "./App.css";
+function App() {
+let dType='template'+2.0
+let anyData="This"+"is Javascript"+dType+"Called template"
+  return (
+    <>
+      <PropsBasic name="Debasish" title="Sahoo" />
+
+      <ValueProps
+      strData="This is String data"
+      numData={123}
+      boolData={false}
+      objData={{name:"Debasish", title:"Sahoo"}}
+      arrData={[1,2,3,4,5,6,7,8,9,10]}
+      jsxData={2+2+2+14-3-(2/3)*(23*5)}
+      anyData={anyData}
+      />
+
+      <PropsPass students={['Arnab','Atanu','Supriya','Shibham','Karan','devjani','Anish']}/>
+    </>
+  );
+}
+
+export default App;
